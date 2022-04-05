58a6f89934371103b7053192e58a3bc1bc4d9f5f
linajja <lina@netmarkuab.com>
2022-04-05 13:05:44 +0300

    change

--- frontend/backend/index.js
+++ frontend/backend/index.js
@@ -1,8 +1,10 @@
-import express, { json } from 'express'
+
+import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 
 const app = express()
+
 app.use(cors())
 
 app.use(express.json())
@@ -11,88 +13,24 @@ app.use(express.urlencoded({
     extended: false
 }))
 
-const database = 'mongodb://localhost/OrderManagement'
-
 mongoose.connect('mongodb://localhost/OrderManagement', (err) => {
     if (!err)
-        console.log("Prisijungimas prie duomenų bazės pavyko")
+        console.log('Prisijungimas prie duomenu bazes pavyko')
 });
+
 const productsSchema = new mongoose.Schema({
     product_name: String,
     description: String,
-    price: String,
-    discount_price: String
+    price: Number,
+    discount_price: Number
 })
 
-const products = mongoose.model('Products', productsSchema, 'Products')
+const Products = mongoose.model('Products', productsSchema, 'Products')
 
-// newPost.content = "Test"
-// newPost.data = '2022-03-30'
-// newPost.save()
+app.get('/show-products', async (req, res) => {
+    const data = await Products.find()
 
-app.get('/show-data', (req, res) => {
-    products.find((err, data) => {
-        if (err)
-            return console.log(err)
-        res.json(data)
-    })
+    res.json(data)
 })
 
-app.delete('/delete-data/:id', (req, res) => {
-    let id = req.params.id
-    products.findByIdAndDelete(id).exec()
-    products.find((err, data) => {
-        if (err)
-            return console.log(err)
-        res.json(data)
-    })
-})
-
-app.post('/save-data', (req, res) => {
-    const newPost = new products()
-    if (
-        req.body.product_name === "" ||
-        req.body.description === "" ||
-        req.body.price === "" ||
-        req.body.discount_price === ""
-    ) {
-        res.send('Užpildykite duomenis')
-
-    } else {
-        newPost.product_name = req.body.product_name
-        newPost.description = req.body.description
-        newPost.price = req.body.price
-        newPost.discount_price = req.body.discount_price
-        newPost.save()
-        res.send({ message: "Failas įrašytas", product_name: req.body.product_name, description: req.body.description, price: req.body.price, discount_price: req.body.discount_price })
-
-    }
-})
-
-// let post = posts.findByIdAndUpdate('6245d03761d2b4a7df2b73f2', {
-//     content: "Programiskai redaguotas irasas"
-// })
-//     .then(data => {
-//         console.log('Irasas sekmingai atnaujintas')
-//     })
-
-
-app.put('/edit-data/:id', (req, res) => {
-    let id = req.params.id
-    let product_name = req.body.product_name
-    products.findByIdAndUpdate(id, {
-        product_name: product_name,
-    })
-        .then(data => {
-            res.send("Info changed")
-        })
-
-})
-
-
-
-
-
-app.listen(5001, () => {
-    console.log('Serveris veikia')
-})
+app.listen(5001)
--- frontend/frontendd/assets/common.js
+++ frontend/frontendd/assets/common.js
@@ -1,7 +1,4 @@
 const url = 'http://localhost:5001'
-const mainInput = document.querySelector('#new-todo')
-const addButton = document.querySelector('#add-new-todo')
-const messageDiv = document.querySelector('.messages')
 
 const transferData = async (url, method = 'GET', data = {}) => {
     let options = {
@@ -10,112 +7,44 @@ const transferData = async (url, method = 'GET', data = {}) => {
             'Content-Type': 'application/json'
         }
     }
+
     if (method != 'GET')
         options.body = JSON.stringify(data)
+
     const resp = await fetch(url, options)
+
     return resp.json()
-}
-
-const getData = () => {
-
-    transferData(url + '/show-data')
-        .then(resp => {
-            let html = '<ul>'
-
-            resp.forEach(value => {
-
-
-                html += `<li data-id="${value._id}">
-                            <span class="product-name">${value.product_name}</span>
-                            <span class="description">${value.description}</span>
-                            <span class="price">${value.price}</span>
-                            <span class="discount-price">${value.discount_price}</span>
-                            <a class="btn btn-danger delete">Delete</a>
-                            <a class="btn btn-primary float-end edit">Edit</a>
-                        </li>`
-            })
-
-            html += '</ul>'
-
-            document.querySelector('#content').innerHTML = html
-
-            document.querySelectorAll('.delete').forEach(element => {
-                let id = element.parentElement.getAttribute('data-id')
-
-                element.addEventListener('click', () => {
-
-                    transferData(url + '/delete-data/' + id, 'DELETE')
-                        .then(resp => {
-                            getData()
-                        })
-
-                })
-            })
-            //     document.querySelectorAll('.edit').forEach(element => {
-
-            //         let id = element.parentElement.getAttribute('data-id')
-            //         let content = element.parentElement.querySelector('.content').textContent
-
-            //         element.addEventListener('click', () => {
-
-            //             mainInput.value = content;
-            //             mainInput.classList.add('edit-mode')
-            //             mainInput.setAttribute('data-mode', 'edit')
-            //             addButton.textContent = addButton.getAttribute('data-edit-label')
-            //             addButton.setAttribute('data-id', id)
-            //         })
-
-            //     })
-
-            //     document.getElementById('add-new-todo').forEach(element => {
-
-            //         let id = element.parentElement.getAttribute('data-id')
-
-            //         element.addEventListener('click', () => {
-
-            //             transferData(url + '/edit-data/' + id, 'PUT')
-            //                 .then(resp => {
-            //                     getData()
-            //                 })
-
-            //         })
-            //     })
-        })
 }
 
 const newOrderForm = async () => {
     const root = document.querySelector('#newOrderForm')
     const productsContainer = root.querySelector('.productSelect')
 
-    const products = await transferData(url + '/show-data')
-
+    const products = await transferData(url + '/show-products')
 
     let html = '<ul>'
 
-    products.forEach(products => {
-        let price = `<span class="normalPrice">${products.price}</span>`
-        if (products.discount_price)
-            price = `<span class="specialPrice">${products.discount_price}</span>
-            <span class="originalPrice">${products.price}</span>`
+    products.forEach(product => {
+        let price = `<span class="normalPrice">${product.price}</span>`
+
+        if (product.discount_price)
+            price = `<span class="specialPrice">${product.discount_price}</span>
+                    <span class="originalPrice">${product.price}</span>`
 
         html += `<li>
-                     <input type="radio" name"product" value="${products._id}>
-                     <div class = "contents">
-                         <div class="name"> ${products.product_name}</div>
-                         <div class="description"> ${products.description}</div>
-                         <div class="price"> ${products.price}</div>
-                 </li>`
+                    <input type="radio" name="product" value="${product._id}">
+                    <div class="contents">
+                        <div class="name">${product.product_name}</div>
+                        <div class="description">${product.description}</div>
+                        <div class="price">${price}</div>
+                    </div>
+                </li>`
     })
 
-    html += '</ul>'
+    html += '<ul>'
 
     productsContainer.innerHTML = html
 }
-
-
-window.addEventListener('load', () => {
-    newOrderForm()
-})
 
 window.addEventListener('load', () => {
     newOrderForm()
